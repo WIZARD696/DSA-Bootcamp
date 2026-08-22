57126eefa6edeab0898080dd92f9fcaf370dd759
Mansiz01 <mansizparihar13@gmail.com>
2026-08-22 23:25:24 +0530

    Time: 6 ms (84.80%) | Memory: 46.9 MB (72.94%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,19 +1,32 @@
 class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            if(map.containsKey(s.charAt(i))){
-                map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-            }
-            else{
-                map.put(s.charAt(i),1);
-            }
+        // HashMap<Character,Integer> map=new HashMap<>();
+        // for(int i=0;i<s.length();i++){
+        //     if(map.containsKey(s.charAt(i))){
+        //         map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        //     }
+        //     else{
+        //         map.put(s.charAt(i),1);
+        //     }
+        // }
+        // for(int i=0;i<s.length();i++){
+        //     if(map.get(s.charAt(i))==1){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        int []count=new int[26];
+        //creating a frequency array..this approach is more optimised
+        for(char c:s.toCharArray()){//remember this function is used to convert the given string into a character array as clear from the name
+            count[c-'a']++;//this will convert the character to the index value of the count array ..you must know it
         }
         for(int i=0;i<s.length();i++){
-            if(map.get(s.charAt(i))==1){
+            if(count[s.charAt(i)-'a']==1){
                 return i;
             }
         }
         return -1;
+
     }
 }
