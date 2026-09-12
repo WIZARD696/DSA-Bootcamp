47a0ccae496561677562e59861d6d09c5ef595ab
Mansiz01 <mansizparihar13@gmail.com>
2026-09-13 03:56:27 +0530

    Time: 2 ms (59.67%) | Memory: 43.4 MB (14.78%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> first=new Stack<>();
+        Stack<Character> second=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='#'){
+                if(first.isEmpty()){
+                    continue;
+                }
+                else{
+                    first.pop();
+                }
+            }
+            else{
+                first.push(ch);
+            }
+        }
+        for(char ch:t.toCharArray()){
+            if(ch=='#'){
+                if(second.isEmpty()){
+                    continue;
+                }
+                else{
+                    second.pop();
+                }
+            }
+            else{
+                second.push(ch);
+            }
+        }
+        while(!first.isEmpty()&&!second.isEmpty()){
+            if(first.pop()!=second.pop()){
+                return false;
+            }
+        }
+        return first.isEmpty()&&second.isEmpty();
+    }
+}
