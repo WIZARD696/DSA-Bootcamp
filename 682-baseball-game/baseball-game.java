6b37708e1c57670a5caa3cf8813fc9d1966486c2
Mansiz01 <mansizparihar13@gmail.com>
2026-09-15 02:44:30 +0530

    Time: 3 ms (79.53%) | Memory: 43.7 MB (30.60%) - LeetSync

---
+++ 682-baseball-game/baseball-game.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack=new Stack<>();
+        for(String ch:operations){
+            if(ch.equals("C")){//''-->
+                stack.pop();
+            }
+            else if(ch.equals("D")){
+                stack.push(2*stack.peek());
+            }
+            else if(ch.equals("+")){
+                stack.push(stack.peek()+stack.get(stack.size()-2));//second last element
+            }
+            else{
+                stack.push(Integer.parseInt(ch));
+            }
+        }
+        int total=0;
+        while(!stack.isEmpty()){
+            total+=stack.pop();
+        }
+        return total;
+    }
+}
