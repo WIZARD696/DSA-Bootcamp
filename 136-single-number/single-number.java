759002f17986018cfdc7fc7764035d2cc1e44b86
Mansiz01 <mansizparihar13@gmail.com>
2026-09-16 03:17:51 +0530

    Time: 14 ms (14.39%) | Memory: 47 MB (28.19%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
+        }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            }
+            else{
+                map.put(nums[i],1);
+            }
+        }
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            if(entry.getValue()==1){
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
